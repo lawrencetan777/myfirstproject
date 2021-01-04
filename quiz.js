ce0b5651a1ba3aca05742d0da77f9d5e14ec8008
lawrencetan777 <lawdafish@gmail.com>
2021-01-04 15:37:12 -0600

    added questions

--- quiz.js
+++ quiz.js
@@ -17,6 +17,22 @@ async function r (){
 var quizzer = await ask("What's your name? ");
  console.log("Hello, " + quizzer)
 
+
+var people = await ask("How many people live with you? (This is going to mess up if you don't put a number like 1,2,3,4 instead of one, two...) ");
+parseInt(people);
+if (people<2) {
+console.log("Your household is too small for us");
+console.log("Bye!!!!")
+rl.close();
+}
+else if (people> 10) {
+console.log("Your household is too big for us");
+console.log("Bye!!!!")
+rl.close();
+}
+else{
+    console.log("Anyone is free to add questions")
+}
 rl.close();
 }
 r();
