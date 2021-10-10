4b3aa3533dee8e6e1303a541621b733221ae5435
Lawrence Tan <lawdafish@gmail.com>
2021-10-09 21:23:38 -0500

    fib calc

--- python apps/app1.py
+++ python apps/app1.py
@@ -1,2 +1,17 @@
 print("Welcome to my first app")
-print("Mwahahahahaha")
+print("fibbonaci calculator")
+
+data={}
+data[0] = 0
+data[1] = 1
+
+def fibonacci(i):
+    if i<0:
+        raise ValueError
+    if i-1 not in data:
+        data[i-1] = fibonacci(i-1)
+    if i-2 not in data:
+        data[i-2] = fibonacci(i-2)
+    return data[i-1] + data[i-2]
+
+print( " fib(100)=" + str(fibonacci(100)))
