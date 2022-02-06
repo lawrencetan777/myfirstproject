a92e3179cbcc59d47cee660f670dcb3f22888474
Lawrence Tan <lawdafish@gmail.com>
2022-02-06 15:41:48 -0600

    a

---
+++ javaapps/app1.java
@@ -0,0 +1,37 @@
+package javaapps;
+class imbored {
+
+    public static void main(String args[]){
+        double height1 = Math.random();
+        height1 = height1 * 2;
+        human bobmccaffery = new human( 24 , height1, 126, "bob mccaffery", "farmer" );
+        double bobheight = bobmccaffery.findH();
+        System.out.println(bobheight);
+
+
+
+
+        
+    }
+    
+}
+class human {
+    int age;
+    double height;
+    int weight;
+    String name;
+    String occupation;
+    public human ( int newage, double newheight, int newweight, String newname, String newoccupation ){
+        age = newage;
+        height =newheight;
+        weight = newweight;
+        name = newname;
+        occupation =newoccupation;
+        
+    }
+    double findH (){
+        return height; 
+    }
+
+
+}
---
+++ javaapps/app2.java
@@ -0,0 +1,29 @@
+package javaapps;
+
+class student{
+    String name;
+    int id;
+    public void setid(int x){
+        id = x;
+    }
+
+
+}
+class Collegestudent extends student{
+    String course;
+    
+    public Collegestudent(){
+        this.course = "blah";
+        
+    }
+    public void setidb(int a){
+        super.setid(a);
+    }
+}
+class a{
+    public static void main( String[] args) {
+        Collegestudent one = new Collegestudent();
+        one.setidb(2354);
+        System .out.println(one.id);
+    }
+}
