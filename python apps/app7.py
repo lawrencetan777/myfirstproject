930ea88dc9afeae21b09bdb4bbe9edc3e52e17cd
Lawrence Tan <lawdafish@gmail.com>
2023-04-04 22:04:40 -0500

    nothing

--- python apps/app7.py
+++ python apps/app7.py
@@ -1,5 +1,5 @@
 import random
-
+diceans = []
 
 dice_1 = [1,5,10,3,7,2]
 dice_2 = [2,3,3,4,3,8]
@@ -8,3 +8,7 @@ dice_4 = [1,2,3,4,5,6]
 def roll(dice):
     value = random.randint(0,5)
     return dice[value]
+
+diceans[1] = input("Choose 1 dice out of 4 ")
+diceans[2] = input("Now choose another" )
+
