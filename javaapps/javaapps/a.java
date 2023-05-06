936dff2e3d5e6775dfe0b006907d2c9ca9f8fdf8
Lawrence Tan <lawdafish@gmail.com>
2023-05-06 14:57:27 -0500

    a

--- javaapps/javaapps/a.java
+++ javaapps/javaapps/a.java
@@ -1,8 +1,13 @@
 package javaapps;
 
 import java.lang.Math;
-import java.util.ArrayList;
-
+import java.util.ArrayList; // List that's implemented from an array
+import java.util.LinkedList; // List that's implemented from a linked list
+import java.util.Set; // interface
+import java.util.Stack;
+import java.util.HashSet; // an implementation of the Set interface, driven by "Hash"
+import java.util.LinkedHashSet; // another implement of the Set interface, driven by "hash" but also maintain order of insertion ("linked")
+import java.util.List; // interface of a "list"
 
 /**
  * 
@@ -10,11 +15,62 @@ import java.util.ArrayList;
 class a {
     
     public static void main(String[] args) {
-        Graph test = generateRandom(5, .2);
+        Graph test = generateRandom(50, .03);
         print(test);
+        int islands = islands(test);
+        System.out.println(islands);
+        
 
     }
+    static ArrayList<Integer> neighbors(Graph g, int i){
+        ArrayList<Integer> neighbors = new ArrayList<>();
+        for( int x = 0; x < g.maxPoint; x++){
+            if(g.edges[i][x] == true){
+                neighbors.add(x);
+            }
+        }
+        return neighbors;
+        
+    }
+    static Set<Integer> DFS(Graph g, int i){
+        ArrayList<Integer> neighbors = new ArrayList<>();
+        Set<Integer> visitedSet = new HashSet<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
 
+        stack.push(i);
+        int v;
+        while(stack.isEmpty() == false){
+            v = stack.pop();
+            visitedSet.add(v);
+            neighbors = neighbors(g,v);
+            for(int n = 0; n < neighbors.size(); n++){
+                if(visitedSet.contains(neighbors.get(n)) == false){
+                    stack.push(neighbors.get(n));
+                }
+            }
+        }
+        return visitedSet;
+    }
+    static int islands(Graph a){
+        int islands = 0;
+
+        Set<Integer> unvisited = new HashSet<>(); // Can check existence in O(1), can add or remove O(1)
+        for(int i = 0; i < a.maxPoint; i++){
+            unvisited.add(i);
+        }
+
+
+
+        for( int i = 0; i < a.maxPoint; i++){
+            if (unvisited.contains(i)) {
+                Set<Integer> visited = DFS(a,i);
+                islands++;
+                unvisited.removeAll(visited);
+                
+            }
+        }
+        return islands;
+    }
     
     
 
@@ -22,14 +78,26 @@ class a {
     static Graph generateRandom(int n, double p) {
         
         Graph a = new Graph();
+        a.edges = new boolean[n][n];
+        a.distance = new int[n][n];
         a.maxPoint = n;
-        for(int z = 0; z < n; z++){
-            for(int x = 0; x < n; x++){
+
+        // 1 3 4 6 7
+        // try to find all pairs that are unique
+        // 13 14 16 17
+        // 34 36 37
+        // 46 47
+        // 67
+
+        
+
+        for(int z = 0; z < n - 1; z++){
+            for(int x = z + 1; x < n; x++){
                 double r = Math.random();
-                if(r <= p){
+                if(r <= p && z != x){
                     a.edges[z][x] = true;
                     a.edges[x][z] = true;
-                    int ar = (int) Math.random()*51;
+                    int ar = (int) (Math.random()*50);
                     a.distance[z][x] = ar;
                     a.distance[x][z] = ar;
                 }else if( a.edges[z][x] != true){
@@ -45,19 +113,20 @@ class a {
 
     // print content of the graph
     static void print(Graph graph){
-        ArrayList<Integer[]> edgeRecord = new ArrayList<>(1);
         Integer[] points = new Integer[2];
-        for(int a = 0; a < graph.maxPoint;a++ ){
-            for(int x = 0; x < graph.maxPoint;x++ ){
+        int edges=0;
+        for(int a = 0; a < graph.maxPoint-1;a++ ){
+            for(int x = a + 1; x < graph.maxPoint;x++ ){
                 points[0] = a;
                 points[1] = x;
-                if( graph.edges[a][x] == true && edgeRecord.contains(points) == false){
- 
+                if( graph.edges[a][x] == true){ 
                     System.out.println("Length " + graph.distance[a][x] + " edge between nodes " + a + " and " + x );
+                    edges++;
                 }
-                
             }
         }
+        System.out.println("Points=" + graph.maxPoint + " edges="+edges);
+
     }
 
     static class Graph {
